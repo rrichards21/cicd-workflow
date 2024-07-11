5b0bb6afe3abd0ec35763357b1c2975dd76c8c23
rrichards21 <rrichards@inf.udec.cl>
2024-07-10 22:03:59 -0400

    test: unittest

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -1,9 +1,8 @@
 import torch
-import os
 from src.inference import load_model, predict
 
 def test_model_output():
-    model = load_model('.\src\doubleit-model.pt')
+    model = load_model('./src/doubleit-model.pt')
     sample_tensor = torch.tensor([1, 2, 3, 4])
     result = predict(model, sample_tensor)
     expected = torch.tensor([2, 4, 6, 8])
